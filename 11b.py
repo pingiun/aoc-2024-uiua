cd956c85e0fbbd965b116a88c2f8e1feed300074
Jelle Besseling <jelle@pingiun.com>
2024-12-11 08:35:37 +0100

    Ideas for 11b

--- 11b.py
+++ 11b.py
@@ -1,20 +1,24 @@
+import math
+import functools
 import tqdm
 
-digits = [int(x) for x in open('11.input').read().strip().split()]
+# digits = [int(x) for x in open('11.input').read().strip().split()]
+
+digits = [5, 62914, 65, 972, 0, 805922, 6521, 1639064]
+
+@functools.cache
+def grow(digit):
+    if digit == 0:
+        return [1]
+    if digit == [1]:
+        return [2024]
+    size = math.floor(math.log10(digit)) + 1
+    if size % 2 == 0:
+        return [digit // (10 ** (size // 2)), digit % (10 ** (size // 2))]
+    return [digit * 1024]
 
 def blink(digits):
-    newdigits = []
-    for digit in digits:
-        if digit == 0:
-            newdigits += [1]
-            continue
-        sdigit = str(digit)
-        slen = len(sdigit)
-        if slen % 2 == 0:
-            newdigits += [int(sdigit[:slen//2]), int(sdigit[slen//2:])]
-            continue
-        newdigits += [digit * 2024]
-    return newdigits
+    return [newdigit for digit in digits for newdigit in grow(digit)]
 
 for _ in tqdm.trange(75):
     print(len(digits))
