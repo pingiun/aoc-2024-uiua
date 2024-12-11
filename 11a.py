e7cc7dc25591dd6368fdca4663dd722025f4ff72
Jelle Besseling <jelle@pingiun.com>
2024-12-11 08:15:46 +0100

    Add 11a and start on 11b

---
+++ 11a.py
@@ -0,0 +1,20 @@
+digits = [int(x) for x in open('11.input').read().strip().split()]
+
+def blink(digits):
+    newdigits = []
+    for digit in digits:
+        if digit == 0:
+            newdigits += [1]
+            continue
+        sdigit = str(digit)
+        slen = len(sdigit)
+        if slen % 2 == 0:
+            newdigits += [int(sdigit[:slen//2]), int(sdigit[slen//2:])]
+            continue
+        newdigits += [digit * 2024]
+    return newdigits
+
+for _ in range(25):
+    digits = blink(digits)
+
+print(len(digits))
