42900c3a2ece46cfe5e69ca309540681b85588ff
Jelle Besseling <jelle@pingiun.com>
2024-12-12 08:49:56 +0100

    Work on 12a

--- 12a.py
+++ 12a.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+from collections import Counter
 
 s = [line for line in open('12.test').read().strip().splitlines()]
 
@@ -14,9 +14,26 @@ def windows3(seq):
     for i in range(len(seq)):
         for j in range(len(seq[0])):
             yield [
-                [get(get(seq, i-1), j-1), get(get(seq, i-1), j), get(get(seq, i-1), j+1)],
-                [get(get(seq, i), j-1), get(get(seq, i), j), get(get(seq, i), j+1)],
-                [get(get(seq, i+1), j-1), get(get(seq, i+1), j), get(get(seq, i+1), j+1)],
+                #get(get(seq, i-1), j-1),
+                get(get(seq, i-1), j), # get(get(seq, i-1), j+1),
+                get(get(seq, i), j-1), get(get(seq, i), j), get(get(seq, i), j+1),
+                # get(get(seq, i+1), j-1),
+                get(get(seq, i+1), j), # get(get(seq, i+1), j+1),
             ]
 
-print(next(windows3(s)))
+totals_counter = Counter()
+
+for window in windows3(s):
+    middle = window[2]
+    print([*window[:2], *window[3:]])
+    counts = Counter([*window[:2], *window[3:]])
+    print(f' {window[0] or " "} ')
+    print(f'{window[1] or " "}{window[2]}{window[3] or " "}')
+    print(f' {window[4] or " "} ')
+    print()
+    del[counts[middle]]
+    if middle == 'R':
+        print(counts)
+    totals_counter[middle] += counts.total()
+
+print(totals_counter)
